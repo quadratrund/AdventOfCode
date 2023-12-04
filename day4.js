5e964e4b356e37bab59f08a0ae16108986d08371
quadratrund <56112624+quadratrund@users.noreply.github.com>
2023-12-04 16:30:11 +0100

    day4

---
+++ day4.js
@@ -0,0 +1,32 @@
+const { readFileSync } = require('fs');
+
+const buffer = readFileSync('input-a4.txt');
+const text = buffer.toString('utf8').split('\n').filter(Boolean);
+
+const cards = text.map(line => {
+    let [ id, numbers ] = line.substring(5).split(':');
+    id = Number(id);
+    const [win, own] = numbers.split('|').map(x => x.trim().split(/ +/).map(Number));
+    const matches = own.filter(x => win.includes(x)).length
+    return { id, win, own, matches };
+});
+//console.log(cards);
+
+// Part 1
+const points = cards
+    .filter(c => c.matches > 0)
+    .map(c => Math.pow(2, c.matches - 1));
+//console.log(points);
+
+const sum = points.reduce((a, b) => a + b);
+console.log(sum);
+
+// Part 2
+cards.forEach(c => c.count = 1);
+cards.forEach((x, i) =>
+    cards
+        .slice(i + 1, i + 1 + x.matches)
+        .forEach(y => y.count += x.count)
+);
+const count = cards.map(x => x.count).reduce((a, b) => a + b);
+console.log(count);
