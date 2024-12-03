6ce6f2077c7455d5e935f7ddd5f4caaba7c011a1
quadratrund <56112624+quadratrund@users.noreply.github.com>
2024-12-03 11:03:30 +0100

    2024 day 3

---
+++ 2024/day3.js
@@ -0,0 +1,35 @@
+const { readFileSync } = require('fs');
+
+const buffer = readFileSync('input-day3.txt');
+const memory = buffer.toString('utf8');
+const regex1 = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const matches1 = Array.from(memory.matchAll(regex1));
+const multiplications1 = matches1.map(match => [Number(match[1]), Number(match[2])]);
+const products1 = multiplications1.map(([a, b]) => a * b);
+const sum1 = products1.reduce((a, b) => a + b);
+console.log(sum1);
+
+// -----
+
+const regex2 = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
+const matches2 = Array.from(memory.matchAll(regex2));
+const multiplications2 = [];
+let enabled = true;
+for (const match of matches2) {
+  switch (match[0]) {
+    case 'do()':
+      enabled = true;
+      break;
+    case 'don\'t()':
+      enabled = false;
+      break;
+    default:
+      if (enabled) {
+        multiplications2.push([Number(match[1]), Number(match[2])])
+      }
+      break;
+  }
+}
+const products2 = multiplications2.map(([a, b]) => a * b);
+const sum2 = products2.reduce((a, b) => a + b);
+console.log(sum2);
