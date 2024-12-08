ac6671169a63bc6a6f85ae56cf1f91c6a2da9dde
quadratrund <56112624+quadratrund@users.noreply.github.com>
2024-12-08 21:07:32 +0100

    2024 day 8

---
+++ 2024/day8.js
@@ -0,0 +1,64 @@
+const { readFileSync } = require('fs');
+
+const map = readFileSync('input-day8.txt').toString('utf8').split('\n').filter(Boolean);
+const allAntennas = {};
+for (const [y, row] of map.entries()) {
+  for (const [x, frequency] of row.split('').entries()) {
+    if (frequency !== '.') {
+      allAntennas[frequency] ??= [];
+      allAntennas[frequency].push({ x, y });
+    }
+  }
+}
+const height = map.length;
+const width = map[0].length;
+const antinodes = new Set();
+function addAntinode(antinode) {
+  const isOnMap = antinode.x >= 0 && antinode.x < width && antinode.y >= 0 && antinode.y < height;
+  if (isOnMap) {
+    antinodes.add(antinode.x + '|' + antinode.y);
+  }
+  return isOnMap;
+}
+for (const antennas of Object.values(allAntennas)) {
+  for (const [indexA, antennaA] of antennas.entries()) {
+    for (const antennaB of antennas.slice(indexA + 1)) {
+      const antinodeA = {
+        x: antennaA.x * 2 - antennaB.x,
+        y: antennaA.y * 2 - antennaB.y
+      }
+      const antinodeB = {
+        x: antennaB.x * 2 - antennaA.x,
+        y: antennaB.y * 2 - antennaA.y
+      }
+      addAntinode(antinodeA);
+      addAntinode(antinodeB);
+    }
+  }
+}
+console.log(antinodes.size);
+
+// -----
+
+function addVectors(a, b) {
+  return {
+    x: a.x + b.x,
+    y: a.y + b.y
+  };
+}
+for (const antennas of Object.values(allAntennas)) {
+  for (const [indexA, antennaA] of antennas.entries()) {
+    for (const antennaB of antennas.slice(indexA + 1)) {
+      addAntinode(antennaA);
+      const vector = {
+        x: antennaA.x - antennaB.x,
+        y: antennaA.y - antennaB.y
+      }
+      for (let antinode = antennaA; addAntinode(antinode); antinode = addVectors(antinode, vector));
+      vector.x *= -1;
+      vector.y *= -1;
+      for (let antinode = antennaB; addAntinode(antinode); antinode = addVectors(antinode, vector));
+    }
+  }
+}
+console.log(antinodes.size);
