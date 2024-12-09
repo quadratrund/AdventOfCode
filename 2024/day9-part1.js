5c9b900ba7faed9b2d15aef800bf3dbe848a7336
quadratrund <56112624+quadratrund@users.noreply.github.com>
2024-12-09 12:44:59 +0100

    2024 day 9

---
+++ 2024/day9-part1.js
@@ -0,0 +1,39 @@
+const { readFileSync } = require('fs');
+
+const map = readFileSync('input-day9.txt').toString('utf8').replaceAll('\n', '').split('').map(Number);
+let movedSize = 0;
+let lastFileIndex = map.length - 1;
+lastFileIndex -= lastFileIndex % 2;
+let position = 0;
+let checkSum = 0;
+for (let [index, size] of map.entries()) {
+  if (index % 2) {
+    for (let i = 0; i < size; i++) {
+      if (lastFileIndex < index) {
+        break;
+      }
+      const lastFileSize = map[lastFileIndex];
+      const lastFileId = lastFileIndex / 2;
+      checkSum += lastFileId * position;
+      movedSize++;
+      if (movedSize >= lastFileSize) {
+        movedSize = 0;
+        lastFileIndex -= 2;
+      }
+      position++;
+    }
+    if (lastFileIndex < index) {
+      break;
+    }
+  } else {
+    const fileId = index / 2;
+    if (index === lastFileIndex) {
+      size -= movedSize;
+    }
+    for (let i = 0; i < size; i++) {
+      checkSum += fileId * position
+      position++;
+    }
+  }
+}
+console.log(checkSum);
---
+++ 2024/day9-part2.js
@@ -0,0 +1,39 @@
+const { readFileSync } = require('fs');
+
+const map = readFileSync('input-day9.txt').toString('utf8').replaceAll('\n', '').split('').map((x, i) =>
+  i % 2
+  ? { type: 'free', size: Number(x) }
+  : { type: 'file', size: Number(x), id: i / 2 }
+);
+for (let i = map.length - 1; i >= 0; i--) {
+  const file = map[i];
+  if (file.type === 'file') {
+    const freeIndex = map.slice(0, i).findIndex(info => info.type === 'free' && info.size >= file.size);
+    if (freeIndex !== -1) {
+      map[i - 1].size += file.size;
+      const thingAfterFile = map[i + 1];
+      if (thingAfterFile?.type === 'free') {
+        map[i - 1].size += thingAfterFile.size;
+        map.splice(i, 2);
+      } else {
+        map.splice(i, 1);
+      }
+      map[freeIndex].size -= file.size;
+      map.splice(freeIndex, 0, file);
+    }
+  }
+}
+
+let position = 0;
+let checkSum = 0;
+for (const info of map) {
+  if (info.type === 'free') {
+    position += info.size;
+  } else {
+    for (let i = 0; i < info.size; i++) {
+      checkSum += info.id * position
+      position++;
+    }
+  }
+}
+console.log(checkSum);
